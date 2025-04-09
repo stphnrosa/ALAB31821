295c9c07f75f1718fd58b9499c43018a692a850c
stphnrosa <stphn.rosa@gmail.com>
2025-04-09 00:07:31 -0400

    added in server

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
+//reference: https://expressjs.com/en/guide/using-template-engines.html
+//reference: https://betterstack.com/community/guides/scaling-nodejs/build-nodejs-application-express-pug/
+
+
 const express = require("express");
 const pug = require("pug");
 const app = express();
 const port = 3000;
 
 //middleware- has accesss to request/response objects 
-app.use(express.static("./styles")); // must use in order to load middleware function
+app.use(express.static(".styles")); // must use in order to load middleware function
+
+app.set("view engine","pug");
 
 //Routes
 app.get("/", (req, res) => {
-    res.send("Home");
+    res.send("This is the homepage!");
 });
+
+const server= app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server: ${server.address().port}`);
+});
+
 
 
 app.get("/about", (req, res) => {
     res.send("about");
 });
 
-//app.listen
+app.listen(port,() => { 
 
-// console.log(`anything: ${port}`);
-// });
-//
+console.log(`Server listening on port:  ${port}`);
+});
